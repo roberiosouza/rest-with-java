d6ce8b7944e9b350588ea9a45207338c866a8dd2
roberio.dev <rroberiosouza@gmail.com>
2023-03-25 16:39:55 -0300

    Implementado customização de exceções

--- Calc-Spring/rest-with-java/src/main/java/br/com/roberio/MathController.java
+++ Calc-Spring/rest-with-java/src/main/java/br/com/roberio/MathController.java
@@ -4,6 +4,8 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
+import br.com.roberio.exceptions.UnsupportedMathOperationException;
+
 @RestController
 public class MathController {
 
@@ -13,7 +15,7 @@ public class MathController {
 						@PathVariable(value = "numberTwo") String numberTwo
 					 ) throws Exception {
 		if (!isNumeric(numberOne) || !isNumeric(numberTwo)) {
-			throw new Exception();
+			throw new UnsupportedMathOperationException("Please set a number value!");
 		}
 		
 		return convertToDouble(numberOne) + convertToDouble(numberTwo);
