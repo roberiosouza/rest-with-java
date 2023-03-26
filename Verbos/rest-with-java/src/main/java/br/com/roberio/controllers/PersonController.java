4fc5ea18bc98e273eed52ed76854a73bdb0ff1bc
roberio.dev <rroberiosouza@gmail.com>
2023-03-26 19:59:10 -0300

    Implementado os verbos basicos: GET, POST, DELETE, PUT. Usando como classe base uma classe que represente uma pessoa.

---
+++ Verbos/rest-with-java/src/main/java/br/com/roberio/controllers/PersonController.java
@@ -0,0 +1,53 @@
+package br.com.roberio.controllers;
+
+import org.springframework.http.MediaType;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+import br.com.roberio.exceptions.UnsupportedMathOperationException;
+import br.com.roberio.model.Person;
+import br.com.roberio.services.PersonService;
+
+@RestController
+@RequestMapping("/person")
+public class PersonController {
+	
+	@Autowired
+	private PersonService service;
+	
+	@GetMapping(value = "/{id}",  produces = MediaType.APPLICATION_JSON_VALUE)
+	public Person findById(@PathVariable(value = "id") String id) {
+		return service.findById(id);
+	}
+	
+	@GetMapping(produces = MediaType.APPLICATION_JSON_VALUE)
+	public List<Person> findAll(){
+		return service.findAll();
+	}
+	
+	@PostMapping(consumes = MediaType.APPLICATION_JSON_VALUE, produces = MediaType.APPLICATION_JSON_VALUE)
+	public Person create(@RequestBody Person person) {
+		return service.create(person);
+	}
+	
+	@PutMapping(consumes = MediaType.APPLICATION_JSON_VALUE, produces = MediaType.APPLICATION_JSON_VALUE)
+	public Person update(@RequestBody Person person) {
+		return service.update(person);
+	}
+	
+	@DeleteMapping(value = "/{id}")
+	public void delete(@PathVariable(value = "id") String id) {
+		service.delete(id);
+	}
+}
